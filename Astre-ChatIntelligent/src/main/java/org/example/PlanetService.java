0133f4e21b3fee522621334c182bb86dfc914038
Dongziqing <115585423+Dongziqing@users.noreply.github.com>
2025-03-20 20:45:57 +0100

    Create PlanetService.java

---
+++ Astre-ChatIntelligent/src/main/java/org/example/PlanetService.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlanetService {
+    public double getScaleFactor(String planet) {
+        return 1.0; //
+    }
+
+    public double getProportion(String planet) {
+        return 1.0; //
+    }
+}
+
